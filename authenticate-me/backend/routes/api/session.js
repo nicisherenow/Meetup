8f7b1625481b7d95bec03465dc8913ec788aa248
Nicholas Talbot <talbotnic26@gmail.com>
2022-11-22 13:58:46 -0700

    login user response complete

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -34,10 +34,16 @@ router.post(
       return next(err);
     }
 
-    await setTokenCookie(res, user);
+    const token = await setTokenCookie(res, user);
 
     return res.json({
-      user: user
+      user: {
+          id: user.id,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          token: token,
+      }
     });
   }
 );
