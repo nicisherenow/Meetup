b78babfc6a93c28bef88f2bcdd831288ed68b007
Nicholas Talbot <talbotnic26@gmail.com>
2022-11-22 12:12:01 -0700

    get current user with required info

--- authenticate-me/backend/db/models/user.js
+++ authenticate-me/backend/db/models/user.js
@@ -5,8 +5,8 @@ const bcrypt = require('bcryptjs');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     toSafeObject() {
-      const { id, firstName, lastName, email, username } = this; // context will be the User instance
-      return { id, firstName, lastName, email, username };
+      const { id, firstName, lastName, email } = this; // context will be the User instance
+      return { id, firstName, lastName, email };
     }
     validatePassword(password) {
       return bcrypt.compareSync(password, this.hashedPassword.toString());
