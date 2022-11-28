9040d222b120fefa437dc2238240414637eb5796
Nicholas Talbot <talbotnic26@gmail.com>
2022-11-27 21:41:38 -0700

    maybe this patch will work

--- authenticate-me/backend/db/models/event.js
+++ authenticate-me/backend/db/models/event.js
@@ -69,7 +69,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     },
     price: {
-      type: DataTypes.FLOAT(7, 2),
+      type: DataTypes.NUMBER(7, 2),
       allowNull: false,
     },
     startDate: {
